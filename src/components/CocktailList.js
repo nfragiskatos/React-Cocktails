6690a8aaeff5fc737db633e77c19bc447e8a6dd6
Nicholas Fragiskatos <Nicholas.Fragiskatos@goldblattsystems.com>
2021-04-28 06:28:12 -0700

    Initial state values
    context.js
    - Added state values for loading, searchTerm and cocktails list
    
    SearchForm.js
    - Retrieved setSearchTerm from state since this component will need to change that when the input changes.
    
    CocktailList.js
    - Retrieved cocktails list and loading flag from global context.
    - Added conditions to handle return for loading state and when cocktails list is empty.

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,14 +1,24 @@
-import React from 'react'
-import Cocktail from './Cocktail'
-import Loading from './Loading'
-import { useGlobalContext } from '../context'
+import React from 'react';
+import Cocktail from './Cocktail';
+import Loading from './Loading';
+import { useGlobalContext } from '../context';
 
 const CocktailList = () => {
-  return (
-    <div>
-      <h2>cocktail list component</h2>
-    </div>
-  )
-}
+	const { cocktails, loading } = useGlobalContext();
 
-export default CocktailList
+	if (loading) {
+		return <Loading />;
+	}
+
+	if (cocktails.length < 1) {
+		return <h2 className="section-title">no cocktails matched your search criteria</h2>;
+	}
+
+	return (
+		<div>
+			<h2>cocktail list component</h2>
+		</div>
+	);
+};
+
+export default CocktailList;
