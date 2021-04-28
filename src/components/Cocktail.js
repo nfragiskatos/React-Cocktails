8a72136a1ed09fe853a52248ac8357c3b2ae198f
Nicholas Fragiskatos <Nicholas.Fragiskatos@goldblattsystems.com>
2021-04-28 07:20:08 -0700

    UI return for Cocktail component from search
    - Received props
    - Created article with image and footer containing cocktail info, as well as a Link component button from react-router-dom to navigate to the single cocktail details page.

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Cocktail = () => {
-  return (
-    <div>
-      <h2>cocktail component</h2>
-    </div>
-  )
-}
+const Cocktail = ({ id, image, name, info, glass }) => {
+	return (
+		<article className="cocktail">
+			<div className="img-container">
+				<img src={image} alt={name} />
+			</div>
+			<div className="cocktail-footer">
+				<h3>{name}</h3>
+				<h4>{glass}</h4>
+				<p>{info}</p>
+				<Link className="btn btn-primary btn-details" to={`/cocktail/:${id}`}>
+					details
+				</Link>
+			</div>
+		</article>
+	);
+};
 
-export default Cocktail
+export default Cocktail;
