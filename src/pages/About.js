7e9710acd0128902bcd11473471b4c67f75de0b6
Nicholas Fragiskatos <Nicholas.Fragiskatos@goldblattsystems.com>
2021-04-28 06:12:01 -0700

    About page implementation

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
-  return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+	return (
+		<section className="section about-section">
+			<h1 className="section-title">about us</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, tempore? Ea unde suscipit
+				accusantium, quae reiciendis fugiat corporis voluptate enim deleniti ipsam vel quasi et quisquam
+				temporibus obcaecati at. Ad fugiat voluptates sapiente nostrum vel ullam qui a voluptate non.
+			</p>
+		</section>
+	);
+};
 
-export default About
+export default About;
