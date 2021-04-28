3f21f19dfd9222591cf9e5433033e71715df5d11
Nicholas Fragiskatos <Nicholas.Fragiskatos@goldblattsystems.com>
2021-04-28 07:06:16 -0700

    Setup data fetch
    - Created async function fetchDrinks to retrieve the cocktail list based off the value of the searchTerm from the API.
    - Mapped the return objects to more usable object if return from API isn't null
    - Set cocktail list global state to either new mapped list or empty array

--- src/context.js
+++ src/context.js
@@ -5,13 +5,44 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-	const [ loading, setLoading ] = useState(false);
+	const [ loading, setLoading ] = useState(true);
 	const [ searchTerm, setSearchTerm ] = useState('a');
 	const [ cocktails, setCocktails ] = useState([]);
 
-	return (
-		<AppContext.Provider value={{ loading, searchTerm, cocktails, setSearchTerm }}>{children}</AppContext.Provider>
+	const fetchDrinks = async () => {
+		setLoading(true);
+		try {
+			const response = await fetch(`${url}${searchTerm}`);
+			const data = await response.json();
+			const { drinks } = data;
+			let newCocktails = [];
+			if (drinks) {
+				newCocktails = drinks.map((drink) => {
+					const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } = drink;
+					return {
+						id    : idDrink,
+						name  : strDrink,
+						image : strDrinkThumb,
+						info  : strAlcoholic,
+						glass : strGlass
+					};
+				});
+			}
+			setCocktails(newCocktails);
+		} catch (error) {
+			console.log(error);
+		}
+		setLoading(false);
+	};
+
+	useEffect(
+		() => {
+			fetchDrinks();
+		},
+		[ searchTerm ]
 	);
+
+	return <AppContext.Provider value={{ loading, cocktails, setSearchTerm }}>{children}</AppContext.Provider>;
 };
 // make sure use
 export const useGlobalContext = () => {
