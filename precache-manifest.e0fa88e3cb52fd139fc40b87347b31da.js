8bafd013a124e8adb5d4ce83ec11aae4acde90dd
root <root@NFrag-Home-Desktop.localdomain>
2021-05-01 07:51:31 -0700

    Updates

---
+++ precache-manifest.e0fa88e3cb52fd139fc40b87347b31da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70ea16736c1d295c9e5fdbbe3fec64f4",
+    "url": "/React-Cocktails/index.html"
+  },
+  {
+    "revision": "f15babbd9f96a63a87e8",
+    "url": "/React-Cocktails/static/css/main.db6e84ad.chunk.css"
+  },
+  {
+    "revision": "e1f6309a6f0d3e350015",
+    "url": "/React-Cocktails/static/js/2.72303b8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Cocktails/static/js/2.72303b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f15babbd9f96a63a87e8",
+    "url": "/React-Cocktails/static/js/main.11521f41.chunk.js"
+  },
+  {
+    "revision": "784912a4d49009080fa5",
+    "url": "/React-Cocktails/static/js/runtime-main.2940f01a.js"
+  },
+  {
+    "revision": "f3ad766494d1f9701717c1a860cd5b10",
+    "url": "/React-Cocktails/static/media/logo.f3ad7664.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React-Cocktails/precache-manifest.e0fa88e3cb52fd139fc40b87347b31da.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Cocktails/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
